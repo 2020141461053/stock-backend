05b18fca33210766dcf7d66fa8cceb6e03884edb
2020141461053 <2287177244@qq.com>
2023-03-08 14:32:46 +0800

    nacos+ratelimit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+o
+?e-backend/src/main/java/com/example/eback/EBackApplication.java,1\9\1915b8c0f024e48c41fcfd0113ac99cf3fbd1113
---
+++ e-backend/src/main/java/com/example/eback/LimitContract/RequestLimitContract.java
@@ -0,0 +1,113 @@
+package com.example.eback.LimitContract;
+
+import com.example.eback.Exception.RequestLimitException;
+import com.google.common.util.concurrent.RateLimiter;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Aspect
+@Component
+public class RequestLimitContract {
+    private static final Logger logger = LoggerFactory.getLogger("requestLimitLogger");
+    private  final Map<String , Integer> IpCount = new HashMap<>();
+
+    private  ConcurrentHashMap<String, RateLimiter> RATE_LIMITER  = new ConcurrentHashMap<>();
+    private RateLimiter rateLimiter;
+
+
+    @Pointcut("@annotation(RateLimit)")
+    public  void RateLimitService(){}
+
+    @Around("RateLimitService()")
+    public Object RateLimitAround(ProceedingJoinPoint point) throws Throwable {
+
+        Signature sig = point.getSignature();
+        //获取拦截的方法名
+        MethodSignature msig = (MethodSignature) sig;
+        //返回被织入增加处理目标对象
+        Object target = point.getTarget();
+        //为了获取注解信息
+        Method currentMethod = target.getClass().getMethod(msig.getName(), msig.getParameterTypes());
+        //获取注解信息
+        RateLimit annotation = currentMethod.getAnnotation(RateLimit.class);
+        double limitNum = annotation.limitNum(); //获取注解每秒加入桶中的token
+        String functionName = msig.getName(); // 注解所在方法名区分不同的限流策略
+
+        RATE_LIMITER.putIfAbsent(functionName, RateLimiter.create(limitNum));
+
+        rateLimiter=RATE_LIMITER.get(functionName);
+        if(rateLimiter.tryAcquire()) {
+            return point.proceed();
+        } else {
+            throw new RuntimeException("服务器繁忙，请稍后再试。");
+        }
+
+    }
+
+
+    @Before("within(@org.springframework.stereotype.Controller *) && @annotation(limit)")
+    public void requestLimit(final JoinPoint joinPoint , RequestLimit limit) throws RequestLimitException {
+        try {
+            Object[] args = joinPoint.getArgs();
+            HttpServletRequest request = null;
+            for (int i = 0; i < args.length; i++) {
+                if (args[i] instanceof HttpServletRequest) {
+                    request = (HttpServletRequest) args[i];
+                    break;
+                }
+            }
+            if (request == null) {
+                throw new RequestLimitException("方法中缺失HttpServletRequest参数");
+            }
+
+            //获取请求中的ip与url链接参数 用于拼接key存放redis中
+            String ip = request.getLocalAddr();
+            String url = request.getRequestURL().toString();
+            String key = "req_limit_".concat(url).concat(ip);
+            if (IpCount.get(key) == null || IpCount.get(key) == 0) {
+                IpCount.put(key, 1);
+            } else {
+                //用于进行ip访问的计数
+                IpCount.put(key, IpCount.get(key) + 1);
+            }
+            int count = IpCount.get(key);
+            if (count > 0) {
+                //创建一个定时器
+                Timer timer = new Timer();
+                TimerTask timerTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        IpCount.remove(key);
+                    }
+                };
+                //这个定时器设定在time规定的时间之后会执行上面的remove方法，也就是说在这个时间后它可以重新访问
+                timer.schedule(timerTask, limit.time());
+            }
+            if (count > limit.count()) {
+                logger.info("用户IP[" + ip + "]访问地址[" + url + "]超过了限定的次数[" + limit.count() + "]");
+                throw new RequestLimitException();
+            }
+        }catch (RequestLimitException e){
+            throw e;
+        }catch (Exception e){
+            logger.error("发生异常",e);
+        }
+    }
+}
